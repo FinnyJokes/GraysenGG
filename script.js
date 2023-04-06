f25dbaa08aa05a900ca24d1ec05d3341a3b0852b
FinnyJokes <86977710+FinnyJokes@users.noreply.github.com>
2023-04-06 17:52:10 +0000

    JavaScript & ReadME

---
+++ script.js
@@ -0,0 +1,13 @@
+window.onload = function() {
+    let wheel = document.querySelector(".wheel");
+    let spinBtn = document.querySelector(".spinBtn");
+    let value = Math.ceil(Math.random() * 3600);
+    spinBtn.onclick = function() {
+      wheel.style.transform = "rotate(" + value + "deg)";
+      value += Math.ceil(Math.random() * 3600);
+      console.log(value);
+    };
+    value.toString();
+    value.parseInt();
+  };
+  //window.open('')
