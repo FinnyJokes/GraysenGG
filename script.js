1fba4b81d32e00303a58e89f4fa257ee8d47983f
FinnyJokes <86977710+FinnyJokes@users.noreply.github.com>
2023-04-06 18:21:49 +0000

    Script

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-window.onload = function() {
-    let wheel = document.querySelector(".wheel");
-    let spinBtn = document.querySelector(".spinBtn");
-    let value = Math.ceil(Math.random() * 3600);
-    spinBtn.onclick = function() {
-      wheel.style.transform = "rotate(" + value + "deg)";
-      value += Math.ceil(Math.random() * 3600);
-      console.log(value);
-    };
-    value.toString();
-    value.parseInt();
-  };
+window.onload = function() { // onload is called when the page is fully loaded
+    let wheel = document.querySelector(".wheel"); // select the wheel element
+    let spinBtn = document.querySelector(".spinBtn"); // select the spin button element
+    let value = Math.ceil(Math.random() * 3600); // generate a random value between 0 and 3600
+    spinBtn.onclick = function() { // when the spin button is clicked
+      wheel.style.transform = "rotate(" + value + "deg)"; // rotate the wheel
+      value += Math.ceil(Math.random() * 3600); // generate a random value between 0 and 3600
+      console.log(value); // log the value to the console
+    }; // end spinBtn.onclick
+    value.toString(); // convert the value to a string
+    value.parseInt(); // convert the value to an integer
+  }; // end window.onload
